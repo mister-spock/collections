0930bafd8d3c319c6d5881286d4d4cb97e323390
Chris Louie <christopher.louie@pgi.com>
2017-04-03 08:36:59 -0700

    feature() Set up basic unit tests

---
+++ tests/iMeetCentral/Collection/SequenceTest.php
@@ -0,0 +1,80 @@
+<?php
+
+use iMeetCentral\Collection\Sequence;
+use PHPUnit\Framework\TestCase;
+
+class SequenceTest extends TestCase {
+
+    /**
+     * @dataProvider mapDataProvider
+     */
+    public
+    function testMap($fn, $input, $expected) {
+        $seq = new Sequence($input);
+
+        $this->assertEquals($seq->map($fn)->to_array(), $expected);
+    }
+
+    public
+    function mapDataProvider() {
+        return [
+            [function($num) { return $num + 1; }, [1, 2, 3, 4], [2, 3, 4, 5]],
+            [function($num) { return $num * $num; }, [1, 2, 3, 4], [1, 4, 9, 16]],
+            [function($num) { return $num * 0; }, [1, 2, 3, 4], [0, 0, 0, 0]],
+        ];
+    }
+
+    /**
+     * @dataProvider filterDataProvider
+     */
+    public
+    function testFilter($fn, $input, $expected) {
+        $seq = new Sequence($input);
+
+        $this->assertEquals($seq->filter($fn)->to_array(), $expected);
+    }
+
+    public
+    function filterDataProvider() {
+        return [
+            [function($num) { return $num < 3; }, [1, 2, 3, 4], [1, 2]],
+            [function($num) { return $num === 'apples'; }, ['apples', 'bananas', 'oranges'], ['apples']],
+        ];
+    }
+
+    /**
+     * @dataProvider takeDataProvider
+     */
+    public
+    function testTake($num, $input, $expected) {
+        $seq = new Sequence($input);
+
+        $this->assertEquals($seq->take($num)->to_array(), $expected);
+    }
+
+    public
+    function takeDataProvider() {
+        return [
+            [0, [1, 2, 3, 4], []],
+            [2, ['apples', 'bananas', 'oranges'], ['apples', 'bananas']],
+        ];
+    }
+
+    /**
+     * @dataProvider dropDataProvider
+     */
+    public
+    function testDrop($num, $input, $expected) {
+        $seq = new Sequence($input);
+
+        $this->assertEquals($seq->drop($num)->to_array(), $expected);
+    }
+
+    public
+    function dropDataProvider() {
+        return [
+            [2, [1, 2, 3, 4], [3, 4]],
+            [1, ['apples', 'bananas', 'oranges'], ['bananas', 'oranges']],
+        ];
+    }
+}
