d70f308da6491f8606a314dc508f4c8abd13316c
Oleg Kozak <okozak@centraldesktop.com>
2018-02-08 13:16:43 +0200

    feature() Simplify creation of "Seq" and "NonRewindableSeq" instances with `Seq::from()` static method
    Issues #APP-15336

--- tests/iMeetCentral/Collection/NonRewindableSeqTest.php
+++ tests/iMeetCentral/Collection/NonRewindableSeqTest.php
@@ -1,15 +1,26 @@
 <?php
 
 use iMeetCentral\Collection\Exception\SeqException;
-use iMeetCentral\Collection\NonRewindableSeq as Seq;
+use iMeetCentral\Collection\NonRewindableSeq as NRSeq;
 use PHPUnit\Framework\TestCase;
 
 class RewindableSeqTest extends TestCase {
 
     public
+    function testFrom() {
+        $testArray   = [1, 2, 3, 4, 5];
+        $resultArray = NRSeq::from($testArray)
+            ->filter('is_int')
+            ->map(function(int $num) { return $num; })
+            ->to_array();
+
+        $this->assertEquals($testArray, $resultArray);
+    }
+
+    public
     function testRewind() {
         $this->expectException(SeqException::class);
-        $seq = new Seq([1]);
+        $seq = new NRSeq([1]);
         $seq->rewind();
     }
 
@@ -17,7 +28,7 @@ class RewindableSeqTest extends TestCase {
     function testReuse() {
         $this->expectException(Exception::class);
 
-        $seq = new Seq([1]);
+        $seq = new NRSeq([1]);
         $seq->to_array();
         $seq->to_array();
     }
